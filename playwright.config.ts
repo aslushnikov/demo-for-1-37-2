d827f1e2436e6ef389b24bb4834049723cd93db4
Andrey Lushnikov <lushnikov@chromium.org>
2023-08-08 17:27:17 +0200

    devops: add merge report

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
 
   /* Configure projects for major browsers */
   projects: [
